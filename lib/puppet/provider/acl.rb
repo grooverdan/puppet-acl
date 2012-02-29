9e38d9ca5e97a0a0204277fa8d7505c890c4cb11
Peter Drake <pdrake@allplayers.com>
2012-02-29 12:10:55 -0500

    Initial cleanup of acl module to make it functional.

--- lib/puppet/provider/acl.rb
+++ lib/puppet/provider/acl.rb
@@ -3,8 +3,4 @@ class Puppet::Provider::Acl < Puppet::Provider
 
   private
 
-  def tempdir
-    @tempdir ||= File.join(Dir.tmpdir, 'acl-' + Digest::MD5.hexdigest(@resource.value(:path)))
-  end
-
 end
